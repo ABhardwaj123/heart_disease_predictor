3ccf72d61c39ecc08cbc5e057a0654108c0b7f26
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2026-06-09 13:12:00 +0530

    some work on frontend

--- frontend/app.js
+++ frontend/app.js
@@ -20,7 +20,7 @@ async function predict(){
     const response = await fetch("http://127.0.0.1:8000/predict", {
         method: "POST",
         headers:{
-            "Content-Type": application/json
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(data)
     })
